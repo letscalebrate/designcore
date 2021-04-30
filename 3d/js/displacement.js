43a7ed4cd50b79f10ba87e0a39aa0f3e6c9ee349
taw7904 <69933201+taw7904@users.noreply.github.com>
2021-04-29 20:02:28 -0400

    resources page, testing mobile and tablet sizing

--- 3d/js/displacement.js
+++ 3d/js/displacement.js
@@ -7,7 +7,6 @@ let slider = document.querySelector("#displacementRange");
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
-  //output.innerHTML = this.value;
-    console.log(this.value);
+  //console.log(this.value);
     img.src = "../assets/3d/displacement-frames/dispalcement" + this.value + ".png";
 }
